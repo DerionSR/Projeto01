04c794c692340eda38989b8bd3840026199da5c0
Derion de Souza Roling <126194167+DerionSR@users.noreply.github.com>
2023-11-15 22:53:32 -0300

    segundo commit
    segundo commit

--- DerionSR/UnitfrmEstado.pas
+++ DerionSR/UnitfrmEstado.pas
@@ -4,10 +4,33 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls;
+  Dialogs, StdCtrls, Grids, DBGrids, DB, Mask, DBCtrls, IBCustomDataSet,
+  IBDatabase, IBTable,;
 
 type
   TCrEstados = class(TForm)
+   CUCancelar: TButton;
+    Button2: TButton;
+    IBDatabase1: TIBDatabase;
+    IBTable1: TIBTable;
+    IBTransaction1: TIBTransaction;
+    IBTable1ID: TIntegerField;
+    IBTable1NOME: TIBStringField;
+    IBTable1UF: TIBStringField;
+    Label1: TLabel;
+    DBEdit1: TDBEdit;
+    Label2: TLabel;
+    DBEdit2: TDBEdit;
+    Label3: TLabel;
+    DBEdit3: TDBEdit;
+    Label4: TLabel;
+    DBEdit4: TDBEdit;
+    DataSource1: TDataSource;
+    CUSalvar: TButton;
+    DBGrid1: TDBGrid;
+    CUExcluir: TButton;
+    CUEditar: TButton;
+    procedure CEEditarClick(Sender: TObject);
     Label1: TLabel;
     Edit1: TEdit;
     Button1: TButton;
@@ -25,5 +48,26 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TCrEstado.CEEditarClick(Sender: TObject);
+begin
+try
+    if MessageBox(handle, 'Deseja editar o registro?','ATENÇÂO!' , mb_YESNo )= mrYes then
+    begin
+        IBtable1.Edit;
+        IBtable1NOME.Value:=DBEdit2.Text;
+        IBtable1UF.Value:=DBEdit3.Text;
+        IBtable1.Post;
+        IBtable1.Close;
+        IBtable1.Open;
+        ShowMessage('Registro Editado!');
+    end;
+except
+      ShowMessage('Erro ao executar atualização!');
+
+end;
+
+end;
+
 
 end.
